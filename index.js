01efb6aa65bc511e14b7bb3e787699f54630171f
Kritika Gupta <kritikagupta1906@gmail.com>
2025-07-08 17:56:09 +0530

    Added Validation and Error Handling

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const Listing = require("./models/listing.js");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 app.engine("ejs", ejsMate);
 
@@ -28,11 +31,22 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if(error) {
+        const errMsg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
+}
+
 // Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 // New Route
 app.get("/listings/new", (req, res) => {
@@ -40,46 +54,54 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
 
     res.render("listings/show.ejs", { listing });
-});
+}));
 
 // Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
-
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 // Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing}, {runValidators : true});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-});
+}));
 
 // Root Route or Home Route
 app.get("/", (req, res) => {
     res.send("This is Home Page of our website WanderLust.");
 });
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Error 404: Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Some Error Occurred." } = err;
+    res.status(status).render("error.ejs", { message });
+})
 
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
--- models/listing.js
+++ models/listing.js
@@ -8,9 +8,12 @@ const listingSchema = Schema({
     },
     description : String,
     image : {
-        type : Object,
-        default : "https://unsplash.com/photos/a-man-standing-on-top-of-a-lush-green-hillside-h1q2CM-at0Q",
-        set : (v) => v === "" ? "https://unsplash.com/photos/a-man-standing-on-top-of-a-lush-green-hillside-h1q2CM-at0Q" : v,
+        filename : String,
+        url : {
+            type : String,
+            default : "https://plus.unsplash.com/premium_photo-1673002094195-f18084be89ce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set : (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1673002094195-f18084be89ce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        }
     },
     price : Number,
     location : String,
